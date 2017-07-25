dc6493266cacabf808c162b25b0e1027d8203cea
Unknown <AlexSabourinDev@gmail.com>
2017-07-25 18:54:25 -0400

    Alex: Placed the hash constants under the Detail namespace
    For consistency, I moved the hash constants to the detail namespace. I also renamed the SHIFT_AMOUNT constant to HASH_SHIFT_AMOUNT in order to avoid name collisions later on.

--- include/utility/Hashing.h
+++ include/utility/Hashing.h
@@ -5,16 +5,16 @@
 
 MIST_NAMESPACE
 
-namespace {
+namespace Detail {
 	const uint32_t BEGIN_HASH_VALUE = 5381;
-	const uint32_t SHIFT_AMOUNT = 5;
+	const uint32_t HASH_SHIFT_AMOUNT = 5;
 }
 
 // Hash a string at compile time, Source on: https://stackoverflow.com/questions/7666509/hash-function-for-string
 // Algorithm Source: https://www.cse.yorku.ca/`oz/hash.html by Dan Bernstein
 // Special thanks to https://www.github.com/xoorath for pointing out that the method presented uses unsigned long which would use a 32 bit integer and not a 64 bit integer.
-constexpr uint32_t Hash_djb2(const char* string, const uint32_t hash = BEGIN_HASH_VALUE) {
-	return (string[0] == '\0') ? 0 : ((hash << SHIFT_AMOUNT) + hash) + string[1] + Hash_djb2(&string[1], hash);
+constexpr uint32_t Hash_djb2(const char* string, const uint32_t hash = Detail::BEGIN_HASH_VALUE) {
+	return (string[0] == '\0') ? 0 : ((hash << Detail::HASH_SHIFT_AMOUNT) + hash) + string[1] + Hash_djb2(&string[1], hash);
 }
 
 MIST_NAMESPACE_END
