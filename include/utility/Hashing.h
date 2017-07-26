469795f4f5d52142b7c6bcabda7aa3c12c02c911
Alex <AlexSabourinDev@gmail.com>
2017-07-26 08:18:05 -0400

    Alex: Removed shift amount constant
    I removed the shift amount constant, it didn't feel like it was serving any purpose or making the code any more readable. If anything it was just obfuscating the code.

--- include/utility/Hashing.h
+++ include/utility/Hashing.h
@@ -7,14 +7,13 @@ MIST_NAMESPACE
 
 namespace Detail {
 	const uint32_t BEGIN_HASH_VALUE = 5381;
-	const uint32_t HASH_SHIFT_AMOUNT = 5;
 }
 
 // Hash a string at compile time, Source on: https://stackoverflow.com/questions/7666509/hash-function-for-string
 // Algorithm Source: http://www.cse.yorku.ca/~oz/hash.html by Dan Bernstein
 // Special thanks to https://www.github.com/xoorath for pointing out that the method presented uses unsigned long which would use a 32 bit integer and not a 64 bit integer.
 constexpr uint32_t Hash_djb2(const char* string, const uint32_t hash = Detail::BEGIN_HASH_VALUE) {
-	return (string[0] == '\0') ? 0 : ((hash << Detail::HASH_SHIFT_AMOUNT) + hash) + string[1] + Hash_djb2(&string[1], hash);
+	return (string[0] == '\0') ? 0 : ((hash << 5) + hash) + string[1] + Hash_djb2(&string[1], hash);
 }
 
 MIST_NAMESPACE_END
